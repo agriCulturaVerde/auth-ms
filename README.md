dcd8f212bd015d432ad7ec5496fd6e392c88e679
miquel febrer <miquelfebrercapo@gmail.com>
2025-12-11 09:35:38 +0100

    se añade la configuracion nats para usuario

---
+++ README.md
@@ -0,0 +1,34 @@
+
+to install class validator and transformer
+```bash
+  npm i class-validator class-transformer
+```
+
+to install microservice
+```bash
+    npm i --save @nestjs/microservices
+```
+
+to install mongo
+```bash
+    npm i @nestjs/mongoose mongoose
+```
+
+to install config
+```bash
+  npm i dotenv
+```
+
+to install validator for configuration
+```bash
+  npm i joi
+```
+##Mount image of Nats en el localhost:8222
+```bash
+  docker run -d --name nats-main -p 4222:4222 -p 8222:8222 nats
+```
+
+##Install nats on the microservice
+```bash
+  npm i --save nats
+```
---
+++ src/user/user.controller.ts
@@ -0,0 +1,33 @@
+import { Controller } from '@nestjs/common';
+import { MessagePattern, Payload } from '@nestjs/microservices';
+import { UserService } from './user.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+
+@Controller()
+export class UserController {
+  constructor(private readonly userService: UserService) { }
+
+  @MessagePattern('createUser')
+  create(@Payload() createUserDto: CreateUserDto) {
+    return createUserDto;
+    // return this.userService.create(createUserDto);
+  }
+
+  @MessagePattern('findUserByEmail')
+  findOne(@Payload() email: string) {
+    return email;
+    // return this.userService.findOne(id);
+  }
+
+  @MessagePattern('updateUser')
+  update(@Payload() updateUserDto: UpdateUserDto) {
+    return updateUserDto;
+    // return this.userService.update(updateUserDto.id, updateUserDto);
+  }
+
+  @MessagePattern('removeUser')
+  remove(@Payload() id: number) {
+    return this.userService.remove(id);
+  }
+}
